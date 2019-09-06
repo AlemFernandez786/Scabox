a4bb97af6438c28bb41cec6dac9a9f04d91f7b21
Alejandro <alemfernandez786@gmail.com>
2019-09-06 19:08:45 -0300

    actualizado

--- materiales.py
+++ materiales.py
@@ -1,7 +1,12 @@
 from PyQt5 import QtWidgets
-
+from Pantallas.Materiales import altaBajaArticulosMaterialesOption
 from Pantallas.Materiales import sectorMateriales
 from Pantallas.Materiales import modificacionMaxMinIngreso
+from Pantallas.Materiales import aprovisionamiento
+from Pantallas.Materiales import consultarStockMateriales
+from Pantallas.Materiales import conteoDeInventarioPorMovil
+from Pantallas.Materiales import modificarStockMateriales
+from Pantallas.Materiales import stockPorMovilMateriales
 import sys
 
 
@@ -16,34 +21,82 @@ class VentanaMateriales(QtWidgets.QMainWindow):
         self.ui.ma_btn_3.clicked.connect(self.alta_baja_stock)
         self.ui.ma_btn_4.clicked.connect(self.maxima_minima_stock)
         self.ui.ma_btn_5.clicked.connect(self.stock_movil)
-        self.ui.ma_btn_6.clicked.connect(self.inventario_stock)
+        self.ui.ma_btn_6.clicked.connect(self.inventario_movil)
         self.ui.ma_btn_7.clicked.connect(self.aprovisionamiento_stock)
         self.ui.ma_btn_cancelar.clicked.connect(self.cancelar)
 
     def consulta_stock(self):
-        print("Consulta")
+        ventanaconsulta = StockMateriales(self)
+        ventanaconsulta.exec_()
 
     def modificacion_stock(self):
-        print("modificacion")
+        ventanamodificacionstock = ModificarStock(self)
+        ventanamodificacionstock.exec_()
 
     def alta_baja_stock(self):
-        print("alta/baja")
+        ventanaalta = AltaBaja(self)
+        ventanaalta.exec_()
 
     def maxima_minima_stock(self):
         ventanamaxmin = MaximaMinima(self)
         ventanamaxmin.exec_()
 
     def stock_movil(self):
-        print("stock x movil")
+        ventanastockmovil = StockPorMovil(self)
+        ventanastockmovil.exec_()
 
-    def inventario_stock(self):
-        print("inventario")
+    def inventario_movil(self):
+        ventanainventario = InventarioMovil(self)
+        ventanainventario.exec_()
 
     def aprovisionamiento_stock(self):
-        print("aprovisionamiento")
+        ventanaaprovisionamiento = Aprovisionamiento(self)
+        ventanaaprovisionamiento.exec_()
 
     def cancelar(self):
         self.close()
+
+
+class AltaBaja(QtWidgets.QDialog):
+    def __init__(self, *args, **kwargs):
+        super(AltaBaja, self).__init__(*args, **kwargs)
+        self.ui = altaBajaArticulosMaterialesOption.Ui_Form()
+        self.ui.setupUi(self)
+
+
+class StockPorMovil(QtWidgets.QDialog):
+    def __init__(self, *args, **kwargs):
+        super(StockPorMovil, self).__init__(*args, **kwargs)
+        self.ui = stockPorMovilMateriales.Ui_Form()
+        self.ui.setupUi(self)
+
+
+class ModificarStock(QtWidgets.QDialog):
+    def __init__(self, *args, **kwargs):
+        super(ModificarStock, self).__init__(*args, **kwargs)
+        self.ui = modificarStockMateriales.Ui_Form()
+        self.ui.setupUi(self)
+
+
+class InventarioMovil(QtWidgets.QDialog):
+    def __init__(self, *args, **kwargs):
+        super(InventarioMovil, self).__init__(*args, **kwargs)
+        self.ui = conteoDeInventarioPorMovil.Ui_Form()
+        self.ui.setupUi(self)
+
+
+class Aprovisionamiento(QtWidgets.QDialog):
+    def __init__(self, *args, **kwargs):
+        super(Aprovisionamiento, self).__init__(*args, **kwargs)
+        self.ui = aprovisionamiento.Ui_Form()
+        self.ui.setupUi(self)
+
+
+class StockMateriales(QtWidgets.QDialog):
+    def __init__(self, *args, **kwargs):
+        super(StockMateriales, self).__init__(*args, **kwargs)
+        self.ui = consultarStockMateriales.Ui_Form()
+        self.ui.setupUi(self)
 
 
 class MaximaMinima(QtWidgets.QDialog):
