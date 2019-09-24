5a9dd6942c7cf586e11b0667be9305e4d9cd683d
niconob92 <54379036+niconob92@users.noreply.github.com>
2019-09-24 20:15:16 -0300

    Add files via upload

---
+++ Serializables/altaBajaArticulosOption.py
@@ -0,0 +1,55 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'altaBajaArticulosOption.ui'
+#
+# Created by: PyQt5 UI code generator 5.13.0
+#
+# WARNING! All changes made in this file will be lost!
+
+
+from PyQt5 import QtCore, QtGui, QtWidgets
+
+
+class Ui_Form(object):
+    def setupUi(self, Form):
+        Form.setObjectName("Form")
+        Form.resize(351, 209)
+        Form.setStyleSheet("background-color:white;")
+        self.label = QtWidgets.QLabel(Form)
+        self.label.setGeometry(QtCore.QRect(10, 10, 331, 51))
+        self.label.setStyleSheet("font-size:20px;")
+        self.label.setObjectName("label")
+        self.se_btn_1 = QtWidgets.QPushButton(Form)
+        self.se_btn_1.setGeometry(QtCore.QRect(10, 120, 151, 31))
+        self.se_btn_1.setStyleSheet("background-color:#5f5fff;\n"
+"color:white;\n"
+"border:none;\n"
+"font-size:15px;")
+        self.se_btn_1.setObjectName("se_btn_1")
+        self.se_btn_2 = QtWidgets.QPushButton(Form)
+        self.se_btn_2.setGeometry(QtCore.QRect(190, 120, 151, 31))
+        self.se_btn_2.setStyleSheet("background-color:#5f5fff;\n"
+"color:white;\n"
+"border:none;\n"
+"font-size:15px;")
+        self.se_btn_2.setObjectName("se_btn_2")
+
+        self.retranslateUi(Form)
+        QtCore.QMetaObject.connectSlotsByName(Form)
+
+    def retranslateUi(self, Form):
+        _translate = QtCore.QCoreApplication.translate
+        Form.setWindowTitle(_translate("Form", "Form"))
+        self.label.setText(_translate("Form", "<html><head/><body><p align=\"center\"><span style=\" font-size:15pt;\">Alta/Baja de Articulos</span></p></body></html>"))
+        self.se_btn_1.setText(_translate("Form", "ALTA"))
+        self.se_btn_2.setText(_translate("Form", "BAJA"))
+
+
+if __name__ == "__main__":
+    import sys
+    app = QtWidgets.QApplication(sys.argv)
+    Form = QtWidgets.QWidget()
+    ui = Ui_Form()
+    ui.setupUi(Form)
+    Form.show()
+    sys.exit(app.exec_())
